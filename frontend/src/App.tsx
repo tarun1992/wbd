992ec446c1f6bd8c3cd7d7d35f21b2b549a9d3de
tarun1992 <tarunnarang1992@gmail.com>
2025-12-06 18:14:37 +0530

    backend setup done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
-import { Link } from 'react-router-dom'
 import React from "react";
+import { Link, Outlet } from 'react-router-dom';
 
 export default function App(){
   return (
-    <div style={{ padding: 20 }}>
-      <h1>WBD User Tracking</h1>
-      <nav>
-        <Link to='/'>Search</Link> | <Link to='/analytics'>Analytics</Link>
-      </nav>
-      <hr/>
-      <div id="content">Open routes: /, /user/:userId, /analytics</div>
+    <div className="container">
+      <header>
+        <h1>WBD User Tracking</h1>
+        <nav>
+          <Link to='/'>Search</Link> | <Link to='/analytics'>Analytics</Link>
+        </nav>
+      </header>
+      <main style={{ marginTop: 16 }}>
+        <div className="card">
+          <Outlet />
+        </div>
+      </main>
     </div>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
-import SearchUser from './pages/SearchUser'
-import UserJourney from './pages/UserJourney'
-import Analytics from './pages/Analytics'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import SearchUser from './pages/SearchUser';
+import UserJourney from './pages/UserJourney';
+import Analytics from './pages/Analytics';
+import './styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App/>} />
-        <Route path='/user/:userId' element={<UserJourney/>} />
-        <Route path='/analytics' element={<Analytics/>} />
+        <Route path="/" element={<App />}>
+          <Route index element={<SearchUser />} />
+          <Route path="user/:userId" element={<UserJourney />} />
+          <Route path="analytics" element={<Analytics />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
